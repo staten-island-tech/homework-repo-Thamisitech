6edc7ac285a40dad30a04f4c6230e36aa22f8d57
Thamisitech <50879028+Thamisitech@users.noreply.github.com>
2019-10-09 09:50:48 -0400

    COMPLETE

--- QUIZ#5TAKEHOME/app.js
+++ QUIZ#5TAKEHOME/app.js
@@ -1,28 +1,17 @@
-const num1 = 100;
-const num2 = 50;
-let val;
+const liList = Array.from(document.querySelectorAll('li'));
+//console.log(liList);
 
-// Simple Math with numbers
+liList.filter (el => el.textContent.includes('Flexbox'));
+//console.log(liList.filter (el => el.textContent.includes('Flexbox')));
 
-val = num1 + num2
-val = num1 * num2
-val = num1 - num2
-val = num1 / num2
-val = num1 % num2
+//console.log(liList.map(el => el.dataset.time));
 
-// Math Object
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.8);
-val = Math.ceil(2.4);
-val = Math.floor(2.8);
-val = Math.sqrt(64);
-val = Math.abs(-2);
-val = Math.pow(4, 2);
-val = Math.min(2, 33, 4, 5, 66, 1, 0.1, 92, -2);
-val = Math.max(22, 45, 82, 91, 03, 33);
-val = Math.random();
-
-val = Math.floor(Math.random() * 20 + 1);  //Random number between 1-20
-
-console.log(val);
+const filtered = liList.filter (el => el.textContent.includes('Flexbox'))
+.map(el => el.dataset.time)
+.map(timeCode => {
+const parts = timeCode.split(':').map( part => parseFloat(part));
+//console.log(parts);
+return (parts[0] * 60) + parts[1];
+})
+.reduce((runningTotal, seconds) => runningTotal + seconds,0)
+console.log(filtered);
