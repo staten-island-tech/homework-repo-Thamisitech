f0eec8a79574d9d59dea62d1463717237c489471
Thamisitech <50879028+Thamisitech@users.noreply.github.com>
2019-10-09 09:26:40 -0400

    QUIZ#5TAKEHOME COMPLETE

---
+++ QUIZ#5TAKEHOME/app.js
@@ -0,0 +1,28 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Simple Math with numbers
+
+val = num1 + num2
+val = num1 * num2
+val = num1 - num2
+val = num1 / num2
+val = num1 % num2
+
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.8);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-2);
+val = Math.pow(4, 2);
+val = Math.min(2, 33, 4, 5, 66, 1, 0.1, 92, -2);
+val = Math.max(22, 45, 82, 91, 03, 33);
+val = Math.random();
+
+val = Math.floor(Math.random() * 20 + 1);  //Random number between 1-20
+
+console.log(val);
