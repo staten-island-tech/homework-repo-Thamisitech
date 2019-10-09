a383f675d2cb2c4404f24cc3cdcf9e81ee5ca49d
Thamisitech <50879028+Thamisitech@users.noreply.github.com>
2019-10-09 09:09:30 -0400

    HW#4COMP

---
+++ HW#4/script.js
@@ -0,0 +1,30 @@
+//Global Scope
+
+var a;
+let b;
+const c = 3;
+
+// function test(){
+// var a = 5;
+// let b = 4;
+// const c = 6;
+// console.log("Function Scope:", a, b, c)
+
+// }
+
+// test();
+
+// if(true){
+//     //Block Scope
+// var a;
+// let b;
+// const c = 3;
+// console.log("If Scope:", a, b, c)
+    
+// }
+
+// for(var a = 0; a< 10; a++ ){
+//    console.log(`loop ${a}`);
+// }
+
+console.log("Global Scope:", a, b, c)
